b75361b712bf0f53bf1f7758f44c7c680d109706
Priscila-ribmiranda <priscila.ribmiranda@gmail.com>
2025-03-04 21:26:57 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+document.addEventListener("DOMContentLoaded", function () {
+    let nomes = [];
+    function adicionarAmigo() {
+        let nomeInput = document.getElementById("amigo");
+        let nome = nomeInput.value.trim();
+        if (nome === "") {
+            alert("Ola, Insira um nome valido");
+            return;
+        }
+        nomes.push(nome);
+        atualizarLista();
+        nomeInput.value = "";
+    }
+
+    function atualizarLista() {
+        let lista = document.getElementById("listaAmigos");
+        lista.innerHTML = "";
+        nomes.forEach(nome => {
+            let li = document.createElement("li");
+            li.textContent = nome;
+            lista.appendChild(li);
+        });
+    }
+
+    function sortearAmigo() {
+        if (nomes.length === 0) {
+            alert("Adicione alguns nomes antes de sortear!");
+            return;
+        }
+        let indiceSorteado = Math.floor(Math.random() * nomes.length);
+        let resultado = document.getElementById("resultado");
+        resultado.innerHTML = "<li>Amigo Secreto: " + nomes[indiceSorteado] + "</li>";
+    }
+    window.adicionarAmigo = adicionarAmigo;
+    window.sortearAmigo = sortearAmigo;
+});
